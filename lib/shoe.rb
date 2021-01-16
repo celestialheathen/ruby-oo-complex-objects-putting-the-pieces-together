3335234f4fb012b5bb26c18f0be350f664050aa9
celestialheathen <jingchaoyu@hotmail.com>
2021-01-16 11:57:57 -0500

    commit tree cb0dd45e956a1917560f0d61e5678925885f8831

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,33 @@
-# Make your shoe class here!
+class Shoe 
+
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand 
+    end 
+
+    def cobble 
+        puts "Your shoe is as good as new!"
+        if self.condition == "old"
+            self.condition = "new"
+        end 
+    end
+
+end
+
+
+  
+#     describe '#cobble' do
+#       it 'says that the shoe has been repaired' do
+#         expect($stdout).to receive(:puts).with("Your shoe is as good as new!")
+#         shoe.cobble
+#       end
+  
+#       it 'makes the shoe\'s condition new' do
+#         shoe.condition = "old"
+#         shoe.cobble
+#         expect(shoe.condition).to eq("new")
+#       end
+#     end
+#   end
